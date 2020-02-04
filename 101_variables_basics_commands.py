e6f58d3625ad77190b2a9cd1492bbdce2088f306
Laila Barradouane <LBarradouane@spartaglobal.com>
2020-02-04 14:56:28 +0000

    doing python variables

---
+++ 101_variables_basics_commands.py
@@ -0,0 +1,38 @@
+# variables in python
+   # Variable is a box. And we put things inside and give it a name.
+
+# assign a variable
+# we gave the box a name and put stuff inside it
+box_pillows = 'pillows and stuff'
+
+# look inside a box
+
+print (box_pillows)
+
+#reassign a vairable
+ # changing the stuff inside the box
+
+box_pillows = 'books'
+print(box_pillows)
+
+# reassign to different dtaa type
+box_pillows = 14
+print(box_pillows)
+# important function in python
+#print()
+
+print('hello hello I am a string')
+print(box_pillows)
+
+# type()
+#returns the data type of the variable/thing inside
+
+print(type('14'))
+print(type(14))
+
+# input()
+ #input prompts use for input
+
+print('Hey there old sport! what is your name?')
+user_response = input()
+print(user_response)
