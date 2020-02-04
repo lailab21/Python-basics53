7d71f34f118d0a22a2d5d108db25dafc4a82b391
Laila Barradouane <LBarradouane@spartaglobal.com>
2020-02-04 15:33:06 +0000

    strings

--- 102_data_types.py
+++ 102_data_types.py
@@ -28,6 +28,39 @@ welcome_message = "welcome to the dangerzone!"
 print(f"{name},welcome to the dangerzone!")
 #place an f behind the string and use {} inside the string to interpolate the variables/python
 
+#special methods for strings
+my_string = "  hello this is an amazing string"
+
+# .count
+print(my_string.count('i'))
+
+# .strip() removes traiiling white space
+print(my_string)
+print(my_string.strip())
+
+# len() - function not a method
+print(len(my_string))
+print(len(my_string.strip()))
+
+#.capitalize()
+print(my_string.strip().capitalize())
+
+# a method to make everything caps
+
+# a method to make everything lower
+# a method to separate the string into several strings.
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.split())
+
+
+
+
+
+
+
+
 
 
 
